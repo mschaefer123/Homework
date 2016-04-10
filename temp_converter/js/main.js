151e429dd0b68bb9e18cee0c17f2cff94c1a9c7f
mschaefer123 <mschaefer123@Matthews-MacBook-Pro.local>
2016-04-10 14:10:15 -0700

    added wireframes

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,22 @@
+// user clicks the #fahrenheit_to_celcius button
+$('#fahrenheit_to_celcius').on('click', function () {
 
+	// Get the value from the #temperature and store in variable
+
+
+	// Convert that value to a number 	
+
+
+	// Convert to celcius and store in variable celcius
+
+	// Display in #result
+	
+
+});
+
+subtract 32 from value
+multiply value x5
+divide value by 9
+
+// user clicks C to F button	
+// 	Multiply by 9, then divide by 5, then add 32
