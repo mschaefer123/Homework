ff15a5cebc7af3cf5f576a72f6f4e39be5e03636
mschaefer123 <mschaefer123@Matthews-MacBook-Pro.local>
2016-04-11 21:25:57 -0700

    FINAL - temp converter
    was not able to get the code to work

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,22 +1,29 @@
 // user clicks the #fahrenheit_to_celcius button
 $('#fahrenheit_to_celcius').on('click', function () {
-
 	// Get the value from the #temperature and store in variable
-
-
-	// Convert that value to a number 	
-
-
-	// Convert to celcius and store in variable celcius
-
-	// Display in #result
+	var temperature = $('#temperature').val(); //Can this be a global variable? to avoid a rewrite in the "C to F button" function
 	
-
+	// Convert that value to a number 	
+	temperature = parseFloat(temperature);
+	
+	// Convert to celcius and store in variable celcius
+	//Subtract 32, then multiply 5, then divide by 9
+	var celcius = (temperature - 32) * 5 / 9; //not sure this line is correct
+	
+	// Display in #result
+	$('#result').html(celcius);
 });
 
-subtract 32 from value
-multiply value x5
-divide value by 9
 
-// user clicks C to F button	
-// 	Multiply by 9, then divide by 5, then add 32
+// user clicks C to F button
+$('fahrenheit_to_celsius').on('click', function () {
+	
+	var temperature = $('#temperature').val();
+	temperature = parseFloat(temperature);
+
+	// Multiply by 9, then divide by 5, then add 32
+	var fahrenheit = temperature * 9 / 5 + 32;
+	
+	// Display in #result
+	$('#result').html(fahrenheit);
+});	
